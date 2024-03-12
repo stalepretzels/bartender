4b89c312fc3015f2268e458a4bddebcdf88ae92c
Clover Johnson <cats11134@gmail.com>
2024-03-12 16:54:23 +0000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,8 @@
-use rustler::{NifEnv, NifTerm, NifResult, NifEncoder};
 use rustrict::CensorStr;
 
-mod atoms {
-    rustler::atoms! {
-        ok,
-        error,
-    }
-}
-
 #[rustler::nif]
-fn filter_profanity(input: String) -> NifResult<String> {
-    let filtered_text = input.censor(input);
-    Ok(filtered_text)
+fn filter_profanity(input: String) -> String {
+    input.censor()
 }
 
-rustler::init!("Elixir.Bartender.Filterer", [filter_profanity], load = |env, _| {
-    Ok(())
-});
+rustler::init!("Elixir.Bartender.Filterer", [filter_profanity]);
